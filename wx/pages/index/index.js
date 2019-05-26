7d0be6aaa88386de8fd122defc743477f4c30e9f
Niunan <besokuser@163.com>
2019-05-26 18:51:27 +0700

    new less proses

---
+++ wx/pages/index/index.js
@@ -0,0 +1,80 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'http://localhost:3000/personalized',
+      method: 'GET',
+      success: (result) => {
+        // console.log(result.data)
+        if(result.data.code === 200){
+          var resList = result.data.result
+          resList = resList.slice(0,6)
+          console.log(resList)
+          this.setData({
+            list:resList
+          })
+        }
+        // result.data
+      },
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
